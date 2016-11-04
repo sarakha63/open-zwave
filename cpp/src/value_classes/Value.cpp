22896c95ac9824452dd847b44b3927d43085f2c2
sarakha63 <sarakha_ludovic@yahoo.fr>
2016-11-04 18:26:47 +0100

    Update Value.cpp

--- cpp/src/value_classes/Value.cpp
+++ cpp/src/value_classes/Value.cpp
@@ -386,6 +386,7 @@ void Value::OnValueRefreshed
 {
 	if( IsWriteOnly() )
 	{
+		Log::Write(LogLevel_Warning, "Value::OnValueRefreshed write only value for node");
 		return;
 	}
 
@@ -416,6 +417,7 @@ void Value::OnValueChanged
 {
 	if( IsWriteOnly() )
 	{
+		Log::Write(LogLevel_Warning, "Value::OnValueChanged write only value for node");
 		return;
 	}
 
